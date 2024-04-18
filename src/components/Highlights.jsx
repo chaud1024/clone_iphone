32674ddefb965c1c4a7b93f7b501af3ee392fba9
chaud1024 <chaud1024@gmail.com>
2024-04-19 00:11:17 +0900

    edit className

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -18,9 +18,9 @@ const Highlights = () => {
   }, []);
 
   return (
-    <section className="w-screen overflow-hidden h-full common-padding bg-zinc">
+    <section className="w-screen h-full overflow-hidden common-padding bg-zinc">
       <div className="screen-max-width">
-        <div className="mb-12 w-full md:flex items-end justify-between">
+        <div className="items-end justify-between w-full mb-12 md:flex">
           <h1 id="title" className="section-heading">
             Get the hightlights.
           </h1>
